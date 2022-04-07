97c0114ff049ae9c2958b2b28df6975751e1fee1
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-04-07 17:24:33 -0400

    P2-001 Created a controller with endpoints

---
+++ practiceTwo2/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace practiceTwo2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult PostUsers()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteUsers()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PutUsers()
+        {
+            return Ok();
+        }
+    }
+}
